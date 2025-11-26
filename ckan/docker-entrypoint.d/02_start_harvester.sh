ca66d1eaa6f7326abfe3759a3ea7b3da05715351
Ole Mussmann <gitlab+account@ole.mn>
2025-11-26 14:39:35 +0100

    use Docker 'secrets' file

--- ckan/docker-entrypoint.d/02_start_harvester.sh
+++ ckan/docker-entrypoint.d/02_start_harvester.sh
@@ -2,8 +2,8 @@
 
 if [[ $CKAN__PLUGINS == *"harvest"* ]]; then
   echo "Starting harvester"
-  supervisorctl -c /home/ckan/supervisor/supervisord.conf start ckan_gather_consumer:ckan_gather_consumer-00
-  supervisorctl -c /home/ckan/supervisor/supervisord.conf start ckan_fetch_consumer:ckan_fetch_consumer-00
+  supervisorctl -c /run/secrets/supervisord_file start ckan_gather_consumer:ckan_gather_consumer-00
+  supervisorctl -c /run/secrets/supervisord_file start ckan_fetch_consumer:ckan_fetch_consumer-00
 
 else
    echo "Not starting harvester"
